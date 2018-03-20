52212b225eea0c966b23638e9c0b7974b2c504e8
kindlehl <hunter.d.lannon@gmail.com>
2018-03-19 23:44:34 -0700

    File hierarchy upgrade

---
+++ source/board.cpp
@@ -0,0 +1,76 @@
+#include "../include/board.h"
+
+
+
+Board::Board(sf::RenderWindow& win, sf::Image img, int w, int h) 
+: width(w), height(h)
+{
+    imageWidth = win.getSize().x;
+    imageHeight = win.getSize().y;
+    for(int i = 0; i < w*h; i++){
+        int x = (i%w)*(imageWidth/w);//generate x coordinate for tile at i
+        int y = (i/w)*(imageHeight/h);//hopefully generates y coordinate
+        tiles.push_back(Tile(win, sf::Rect<int>(x,y,imageWidth/w, imageHeight/h), img, i));
+
+    }
+    
+    (--tiles.end())->disable(); //remove bottom right corner from puzzle
+    blank = (--tiles.end())->OrderNumber; //mark bottom right as blank
+
+//ITS SCRAMBLING TIME
+    randomize(tiles);
+}
+
+void Board::randomize(std::vector<Tile>& tiles){
+    srand(time(NULL));
+
+    for(int i = 0; i < imageWidth; i++){
+        for(int j = 0; j < imageHeight; j++){
+            Board::move(rand()%imageWidth, rand()%imageHeight);
+        }
+    }
+}
+
+//gets indexes that the clicked tile could move to, given an empty space
+std::set<int> Board::getMoves(){
+    std::set<int> s;
+    s.insert(clicked-1); //tile to left
+    s.insert(clicked+1); //tile to right
+    s.insert(clicked-width);//tile up
+    s.insert(clicked+width);//tile down
+    return s;
+}
+
+bool Board::checkMoves(){
+    //if the clicked tile could move to blank tile return true
+    return (possibleMoves.find(blank) != possibleMoves.end());
+}
+
+void Board::swap(Tile& t1, Tile& t2){
+    std::swap(t1.OrderNumber, t2.OrderNumber);
+    std::swap(t1.active, t2.active);
+    t1.texTile.setTexture(t2.texture.get());
+    t2.texTile.setTexture(t1.texture.get());
+    std::shared_ptr<sf::Texture> temp = t1.texture;
+    t1.texture = t2.texture;
+    t2.texture = t1.texture;
+}
+
+void Board::move(int x, int y){
+   //calculate index at (x,y)
+    clicked = (width*x/imageWidth)+width*(height*y/imageHeight);
+   //generate possibleMoves
+    possibleMoves = getMoves();
+   //check if move is valid
+    if(checkMoves()){
+//        valid move, swap tiles
+        //std::cout << "CLICKED: " <<  clicked << std::endl;
+        //std::cout << "BLANK: " << blank << std::endl;
+        Board::swap(tiles[blank], tiles[clicked]);
+        blank=clicked;
+    }
+    //else
+        //std::cout << "ERROR: Invalid Move!" << std::endl;
+
+}
+
---
+++ source/main.cpp
@@ -0,0 +1,57 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "../include/tile.h"
+#include "../include/board.h"
+#include <math.h>
+
+using namespace std;
+
+void processEvents(sf::RenderWindow& window, Board& board);
+
+
+int main(int argc, char** argv){
+    int height, width;
+    height = width = 600;
+    sf::Image Img; 
+   if(argc != 2){
+        cout << "Invalid Arguments. Please enter the name of a file or image on the internet" << endl;
+        return -1;
+    }
+    if(!Img.loadFromFile(argv[1])){
+        cout << "ERROR OPENING FILE: " << argv[1] << " ABORTING!" << endl;
+        return -1;
+    }
+    width = Img.getSize().x;
+    height = Img.getSize().y;
+
+    sf::RenderWindow window(sf::VideoMode(width, height), "Puzzle Project");
+    sf::RectangleShape background(sf::Vector2f(width, height));
+    background.setFillColor(sf::Color(100, 15, 60));
+    window.setFramerateLimit(60);
+    Board board(window, Img, 3, 3);
+    while(window.isOpen()){
+        window.clear();
+        processEvents(window, board);
+        window.draw(background);
+        board.draw();
+        window.display();
+    }
+
+    return 0;
+
+}
+
+
+void processEvents(sf::RenderWindow& window, Board& board){
+     sf::Event event;
+     while(window.pollEvent(event)){
+
+         if(event.type==sf::Event::Closed)
+                window.close();
+         if(event.type==sf::Event::MouseButtonPressed){
+            board.move(event.mouseButton.x, event.mouseButton.y);
+         }
+     }
+
+}
+
